5dc612dc33286630d757cae56bf50c9ac7c3c0b5
madelshafek <39400363+madelshafek@users.noreply.github.com>
2022-11-10 11:51:45 +0200

    sequesnce

---
+++ src/main/java/com/dsa/training/problems/sequence.java
@@ -0,0 +1,60 @@
+package com.dsa.training.problems;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.List;
+
+//@SpringBootApplication
+public class sequence implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(sequence.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        List arr = Arrays.asList(3, 5, -4, 8, 11, 1, -1, 6);
+        List seq = Arrays.asList(3, 11, 6);
+        System.out.println(isValidSubsequence(arr, seq));
+    }
+
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+
+        if (array.isEmpty() || sequence.isEmpty()) {
+            return false;
+        }
+        int seqIndex = 0;
+        int arrIndex = 0;
+        while (arrIndex < array.size() && seqIndex < sequence.size()) {
+
+            if (array.get(arrIndex).equals(sequence.get(seqIndex))) {
+                seqIndex++;
+            }
+            arrIndex++;
+        }
+        return seqIndex == sequence.size();
+    }
+
+
+    public static boolean isValidSubsequence1(List<Integer> array, List<Integer> sequence) {
+
+        if (array.isEmpty() || sequence.isEmpty()) {
+            return false;
+        }
+        int index = 0;
+        for (Integer num : array) {
+            if (index == sequence.size()) {
+                break;
+            }
+            if (num == sequence.get(index)) {
+                index++;
+            }
+        }
+        return index == sequence.size();
+    }
+
+}
