f9b0b6d9b2202cead0f4b59ba01f17d5800b111b
madelshafek <39400363+madelshafek@users.noreply.github.com>
2022-11-10 11:51:15 +0200

    sequesce

--- src/main/java/com/dsa/training/problems/set.java
+++ src/main/java/com/dsa/training/problems/set.java
@@ -1,8 +1,7 @@
-package com.dsa.training;
+package com.dsa.training.problems;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.ArrayList;
 import java.util.Collections;
---
+++ src/main/java/com/dsa/training/problems/twonumbersum.java
@@ -0,0 +1,69 @@
+package com.dsa.training.problems;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.List;
+
+//@SpringBootApplication
+public class twonumbersum implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(twonumbersum.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+//		System.out.println(factorial(4));
+//		System.out.println(fib(4));
+//		System.out.println(rev("1234"));
+//		System.out.println(revit("1234"));
+//        List result = new ArrayList<Object>();
+//        result.add(1);
+//        List arr = new ArrayList<Object>();
+//        arr.add(2);
+//        arr.add(2);
+//        result.add(arr);
+        int arr[] = {3, 5, -4, 8, 11, 1, -1, 6};
+        System.out.println(Arrays.toString(twoNumberSum(arr, 10)));
+    }
+
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+        Hashtable<Integer, Integer> ht1 = new Hashtable<>();
+        for (int i = 0; i < array.length; i++) {
+            int remainder=targetSum-array[i];
+            if(ht1.containsKey(remainder)){
+                int arr[] = {array[i], ht1.get(remainder)};
+                return arr;
+            }else
+            {
+                ht1.put(array[i],array[i]);
+            }
+
+
+
+        }
+
+        return new int[0];
+    }
+
+    public static int[] twoNumberSum1(int[] array, int targetSum) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] + array[j] == targetSum) {
+                    int arr[] = {array[i], array[j]};
+                    return arr;
+                }
+            }
+
+        }
+
+
+        // Write your code here.
+        return new int[0];
+    }
+}
